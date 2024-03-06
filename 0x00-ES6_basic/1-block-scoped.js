4ff47f84e94c7b5b20821c72bf18342243cecba0
Mehdi <mehdi.rabbai@gmail.com>
2024-03-06 20:32:07 +0000

    add correction for task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = true;
+    var task2 = false;
   }
 
+  var task = false;
+  var task2 = true;
   return [task, task2];
 }
