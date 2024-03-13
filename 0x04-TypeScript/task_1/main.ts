8ba2ef774c4d62f310abc9d95112ef55fe7e27a6
mehdi <mehdi.rabbai@gmail.com>
2024-03-13 22:41:04 +0000

    add task 2

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -1,4 +1,4 @@
-interface Teacher {
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
---
+++ 0x04-TypeScript/task_2/main.ts
@@ -0,0 +1,14 @@
+import { Teacher } from '../task_1/main';
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
